f13f2f27158b662fa207016495c9de64c808bfdb
Aashiq Abrahams <aashiqabrahamszb@gmail.com>
2019-03-16 21:36:47 +0200

    classes and interfaces added

--- src/main/java/com/aashiq/cput/App.java
+++ src/main/java/com/aashiq/cput/App.java
@@ -1,5 +1,7 @@
 package com.aashiq.cput;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,39 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scan = new Scanner(System.in);
+
+        //Encapsulation
+        Mammal mammal = new Mammal(4);
+        System.out.println(mammal.getDetails());
+
+        press(scan);
+
+        //Inheritance
+        Animal giraffe  = new Animal("Melman", "Giraffe", 13, 11, 1994, 4);
+        System.out.println(giraffe.toString());
+        System.out.println(giraffe.getDetails());
+
+        press(scan);
+
+        //Polymorphism
+        Herbivore h = giraffe;
+        System.out.println(h.diet);
+
+        press(scan);
+
+        //Composition
+        AnimalName name = new AnimalName("Julian", "Leema");
+        DateOfRegistration dor = new DateOfRegistration(25, 11, 1995);
+        AnimalComp julian = new AnimalComp(name, dor);
+        System.out.println(julian.toString());
+
+    }
+
+    public static void press(Scanner scan)
+    {
+        System.out.println("\nPress enter to continue\n");
+        String y = scan.nextLine();
+    }
     }
 }
---
+++ src/main/java/com/aashiq/cput/Herbivore.java
@@ -0,0 +1,7 @@
+package com.aashiq.cput;
+
+public interface Herbivore {
+
+    public String diet = "Plant life is the right life!";
+
+}
